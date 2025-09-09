3fb5394335abf508d05bfc46adcee3ec716826df
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-09-09 16:40:21 +0800

    day02.java

---
+++ day02.java
@@ -0,0 +1,18 @@
+public class day02 {
+    public static void main(String[] args) {
+  
+        System.out.print("Wiwi"); 
+//cetak "wiwi" tanpa pindah baris
+
+        System.out.println("");
+//untuk baris kosong
+
+        System.out.println("Wiwi");
+ // Cetak "Wiwi" dan pindah baris
+
+        String nama = "Wiwi";
+        int umur = 21;
+
+        System.out.printf("nama saya %s, umur %d tahun", nama, umur);
+    }
+}
