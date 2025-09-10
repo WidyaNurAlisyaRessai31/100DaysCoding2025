27ce6ed9f772b776353808ceb64f3966e996059f
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-09-10 12:58:04 +0800

    day03.java

---
+++ day03.java
@@ -0,0 +1,14 @@
+public class day03 {
+    public static void main(String[] args) {
+      System.out.println("cara menggunakan komentar dalam kodingan yaitu dengan menambahkan (//)");
+      //fungsi dari "//" dalam kodingan adalah untuk memberikan sebuah komentar atau keterangan dari ssebuah kodingan
+      //ini adalah contoh komentar
+      /*Ini adalah contoh komentar multiline Bisa dipakai
+        untuk menjelaskan kode dalam beberapa baris.        
+        */
+      
+      String nama = "Wiwi";
+      System.out.println("Nama:" + nama); //akan mencetak "Nama:" dan variabeln nama"
+
+    }
+}
