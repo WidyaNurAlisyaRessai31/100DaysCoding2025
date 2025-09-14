9c00b78cc503cfa704a333940cdb94512be0a765
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-09-14 22:05:40 +0800

    day07.java

---
+++ day07.java
@@ -0,0 +1,16 @@
+public class Day07 {
+    public static void main(String[] args) {
+      // Tipe data char
+      char inisialNama = 'W';
+      char simbol ='*'
+      
+      // Tipe data boolean
+        boolean B= true;
+        boolean S= false;
+
+      // Tampilkan informasi
+        System.out.println("Inisial Nama      :" + inisialNama);
+        System.out.println("Simbol khusus     :" + simbol);
+        System.out.println("Yang Diitampilkan :" + B);
+        System.out.println("Yang Diitampilkan :" + S);
+    }
