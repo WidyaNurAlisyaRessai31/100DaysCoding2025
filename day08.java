97e96b44cd3773ef34804795798646f618266bbf
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-09-15 20:36:43 +0800

    day08.java

---
+++ day08.java
@@ -0,0 +1,9 @@
+public class day08 {
+    public static void main(String[] args) {
+                String nama ="Wiwi";
+                String favFood =" sushi ";
+                
+                System.out.println("Hii, nama saya "+nama);
+                System.out.println("Makanan favourite saya "+ favFood);
+    }
+}
