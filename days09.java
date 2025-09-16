42d97c972ef9870a6d8a791a5c1fd09a99139a60
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-09-16 20:25:59 +0800

    days09.java

---
+++ days09.java
@@ -0,0 +1,11 @@
+public class Day9 {
+   
+    public static void main(String[] args) {
+       int angka = 31;
+        System.out.println("Sebelum update:");
+        System.out.println("Nilai angka : " + skor);
+        angka = 50;
+        System.out.println("Setelah update:");
+        System.out.println("Nilai angka: " + skor);
+    }
+}
