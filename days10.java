238d0bff8288d69ffb52eac1159fd2673c06e827
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-09-17 22:26:41 +0800

    days10.java

---
+++ days10.java
@@ -0,0 +1,10 @@
+public class days10 {
+    public static void main(String[] args) {
+        final double PI = 3.14;   // ← final = tidak bisa diubah
+        System.out.println(PI);   // tampil: 3.14
+
+//konstanta adalah sebuah variabel yang nilainya tetap atau tidak bisa diubah
+      
+        // PI = 4.0;   //  Kalau ini dihapus komentarnya -> ERROR
+    }
+}
