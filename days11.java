117e421cc1cef6b571a8c30911272ce49c1babb0
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-09-18 23:05:16 +0800

    days11.java

---
+++ days11.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class days11  {
+    public static void main(String[] args) {
+        Scanner wiwi = new Scanner (System.in);
+        System.out.print("Masukkan Nama:");
+        String nama = wiwi.nextLine();
+        System.out.print("Masukkan Umur:");
+        int umur = wiwi.nextInt();
+        System.out.print("Masukkan Tinggi Badan:");
+        int tinggi_badan = wiwi.nextInt();
+
+
+
+        System.out.println("Nama :" + nama);
+        System.out.println("Umur :" + umur);
+        System.out.println("Tinggi Badan :" + tinggi_badan);
+
+
+    }
+}
