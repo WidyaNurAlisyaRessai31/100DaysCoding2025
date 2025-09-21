8e2f116e38d49eab702c2f62e4038e5fcae3bcb4
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-09-21 20:30:36 +0800

    days14.java

---
+++ days14.java
@@ -0,0 +1,12 @@
+public class days14 {
+    public static void main(String[] args) {
+        // String awal
+        String strAngka = "123";
+
+        // Konversi String ke int
+        int angka = Integer.parseInt(strAngka);
+
+        // Output hasil
+        System.out.println("Hasil konversi: " + angka);
+    }
+}
