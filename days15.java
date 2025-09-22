f63d16cf8e6e061b891bcd839682783cb195579d
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-09-22 22:53:54 +0800

    days15.java

---
+++ days15.java
@@ -0,0 +1,12 @@
+public class days15 {
+    public static void main(String[] args) {
+        int angka1 = 20;
+        int angka2 = 5;
+
+        int jumlah = angka1 + angka2;
+        int kurang = angka1 - angka2;
+
+        System.out.println("Hasil Penjumlahan: " + jumlah);
+        System.out.println("Hasil Pengurangan: " + kurang);
+    }
+}
