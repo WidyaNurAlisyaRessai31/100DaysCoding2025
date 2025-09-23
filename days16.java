66ea75a2a6a947b533eab1af20f651a17d7e98fe
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-09-23 21:03:28 +0800

    days16.java

---
+++ days16.java
@@ -0,0 +1,18 @@
+public class days16 {
+    public static void main(String[] args) {
+        // Inisialisasi variabel
+        int a = 20;
+        int b = 4;
+
+        // Perkalian
+        int hasilKali = a * b;
+        // Pembagian
+        int hasilBagi = a / b;
+
+        // Tampilkan hasil
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Hasil Perkalian a * b = " + hasilKali);
+        System.out.println("Hasil Pembagian a / b = " + hasilBagi);
+    }
+}
