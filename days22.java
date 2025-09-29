0b2ec1f2c2236af54973e2303315f14b95e33b15
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-09-29 22:11:03 +0800

    days22.java

---
+++ days22.java
@@ -0,0 +1,8 @@
+public class days22 {
+    public static void main(String[] args) {
+        int sisi = 6;
+        int luas = sisi * sisi;
+        System.out.println("sisi :"+sisi);
+        System.out.println("luas :"+luas);
+    }
+}
