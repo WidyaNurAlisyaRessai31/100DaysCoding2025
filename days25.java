e98577a67bcfdc2a6a08f2e3d09be78ebd90bf92
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-10-02 21:42:19 +0800

    days25.java

---
+++ days25.java
@@ -0,0 +1,13 @@
+public class days25 {
+    public static void main(String[] args) {
+        int angka = 5; // nilai awal
+
+        System.out.println("Nilai awal: " + angka);
+
+        angka++; // naik 1
+        System.out.println("Setelah angka++: " + angka);
+
+        angka--; // turun 1
+        System.out.println("Setelah angka--: " + angka);
+    }
+}
