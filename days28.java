b2b35ca7facea9981bc73dbfc2331fb7aaf77ee1
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-10-05 14:35:08 +0800

    days28.java

---
+++ days28.java
@@ -0,0 +1,19 @@
+public class days28 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 10;
+
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c);
+
+        // Operator ==
+        System.out.println("Apakah a == b? " + (a == b)); // false
+        System.out.println("Apakah a == c? " + (a == c)); // true
+
+        // Operator !=
+        System.out.println("Apakah a != b? " + (a != b)); // true
+        System.out.println("Apakah a != c? " + (a != c)); // false
+    }
+}
