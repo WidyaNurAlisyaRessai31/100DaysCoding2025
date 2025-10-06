0faa66093c0f9949bf805fcff59a65a4233f1a03
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-10-06 20:29:48 +0800

    days29.java

--- days29.java
+++ days29.java
@@ -1,20 +1,8 @@
 public class days29 {
     public static void main(String[] args) {
-        int angka1 = 15;
-        int angka2 = 20;
-
-        // Operator perbandingan <
-        boolean hasilKurangDari = angka1 < angka2; 
-        // true karena 15 lebih kecil dari 20
-
-        // Operator perbandingan >
-        boolean hasilLebihDari = angka1 > angka2; 
-        // false karena 15 tidak lebih besar dari 20
-
-        System.out.println("Nilai angka1 = " + angka1);
-        System.out.println("Nilai angka2 = " + angka2);
-
-        System.out.println("Apakah angka1 < angka2? " + hasilKurangDari);
-        System.out.println("Apakah angka1 > angka2? " + hasilLebihDari);
+        int x = 8;
+        int y = 3;
+        System.out.println(x < y);
+        System.out.println(x > y);
     }
 }
