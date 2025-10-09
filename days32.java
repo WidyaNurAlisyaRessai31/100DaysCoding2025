f45d301196ed0857652a96b1413426c1317c724d
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-10-09 19:13:37 +0800

    days32.java

---
+++ days32.java
@@ -0,0 +1,14 @@
+public class days32 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+
+        // Operator logika OR (||)
+        // Kondisi akan bernilai true jika salah satu bernilai true
+        if (x > 8 || y < 3) {
+            System.out.println("Salah satu kondisi benar.");
+        } else {
+            System.out.println("Kedua kondisi salah.");
+        }
+    }
+}
