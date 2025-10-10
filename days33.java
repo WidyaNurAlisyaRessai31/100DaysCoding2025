4f16406ecf724d724056129d83e16f754d9d88e2
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-10-10 16:40:24 +0800

    days33.java

---
+++ days33.java
@@ -0,0 +1,11 @@
+public class days33 {
+    public static void main(String[] args) {
+        int angka = 10;
+
+        // Mengecek apakah angka bukan nol menggunakan NOT
+        boolean bukanNol = !(angka == 0);
+
+        System.out.println("Angka: " + angka);
+        System.out.println("Apakah angka bukan nol? " + bukanNol);
+    }
+}
