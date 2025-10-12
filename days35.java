eab7a802ac4b4d0f651298af8bfa71ecd22eab6b
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-10-12 20:57:08 +0800

    days35.java

---
+++ days35.java
@@ -0,0 +1,14 @@
+public class days35 {
+    public static void main(String[] args) {
+        int nilai = 60; 
+
+     
+        if (nilai >= 70) {
+            System.out.println("Selamat, kamu lulus!");
+        } else {
+            System.out.println("Maaf, kamu belum lulus.");
+        }
+
+        System.out.println("Program selesai.");
+    }
+}
