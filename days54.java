56723c7df51fdd40acbcefaeada37b3366bb500b
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-10-31 21:55:15 +0800

    days54.java

---
+++ days54.java
@@ -0,0 +1,10 @@
+public class days54 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 6) {
+                continue; 
+            }
+            System.out.println("Hari ke-" + i);
+        }
+    }
+}
