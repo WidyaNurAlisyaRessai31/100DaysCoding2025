05b2adceba03df82a54cbb2746312408dd49fba5
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-11-02 19:50:12 +0800

    days56.java

---
+++ days56.java
@@ -0,0 +1,11 @@
+public class days56{
+    public static void main(String[] args) {
+        
+        int hitungan = 1;
+        do {
+            System.out.println("Angka: " + hitungan);
+        } 
+        while (hitungan <= 5);
+        System.out.println("Perulangan selesai.");
+    }
+}
