bb36a4e573c79c9ceee421ac5090d01dae623aff
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-11-04 18:23:06 +0800

    days58.java

---
+++ days58.java
@@ -0,0 +1,9 @@
+public class days58 {
+        public static void main(String[] args) {
+            int N = 5;
+            System.out.println("Angka 1 sampai " + N + ":");
+            for (int i = 1; i <= N; i++) {
+                System.out.print(i);
+            }
+        }
+    }
