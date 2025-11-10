6b8e33c37998c924a02407f6f83edb6c4de3f66d
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-11-10 22:12:45 +0800

    days64.java

---
+++ days64.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class days64 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+       
+        System.out.print("Masukkan nilai M: ");
+        int M = input.nextInt();
+        
+        System.out.print("Masukkan nilai N: ");
+        int N = input.nextInt();
+        
+        int hasil = 1;
+        for (int i = 1; i <= N; i++) {
+            hasil *= M;
+        }
+        
+        System.out.println(M + " pangkat " + N + " = " + hasil);
+        
+        
+    }
+}
