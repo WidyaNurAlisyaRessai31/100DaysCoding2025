6466b465fcb1eb9b5d94c3d50b86f3b4d0b333d5
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-11-26 19:03:22 +0800

    days80.java

---
+++ days80.java
@@ -0,0 +1,10 @@
+public class days80 {
+    public static void main(String[] args) {
+        
+        int[] angka = {10, 20, 30, 40, 50};
+        System.out.println("Isi Array 1D:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Index " + i + " = " + angka[i]);
+        }
+    }
+}
