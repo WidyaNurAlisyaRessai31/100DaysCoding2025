c998224f00b5c256e91e3c519a79a7d96f384526
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-11-27 19:29:06 +0800

    days81.java

---
+++ days81.java
@@ -0,0 +1,12 @@
+public class days81 {
+    public static void main(String[] args) {
+        int[] angka = {10, 20, 30, 40, 50};
+        int pertama = angka[0];
+        int ketiga = angka[2];
+        int terakhir = angka[4];
+
+        System.out.println("Elemen pertama: " + pertama);
+        System.out.println("Elemen ketiga: " + ketiga);
+        System.out.println("Elemen terakhir: " + terakhir);
+    }
+}
