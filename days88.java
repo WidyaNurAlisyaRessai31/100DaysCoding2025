7cecd9a4a14d5abf1c3f11554e44450d17d747f5
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-12-04 22:15:33 +0800

    days88.java

---
+++ days88.java
@@ -0,0 +1,10 @@
+public class days88 {
+
+    public static void main(String[] args) {
+
+        int[] bilangan = {10, 20, 30, 40, 50};
+        for (int angka : bilangan) {
+            System.out.println("Elemen Array: " + angka);
+        }
+    }
+}
