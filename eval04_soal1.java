07404a14ce25be6ee716446bdcae68c4b7c7ec34
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-12-04 22:39:58 +0800

    eval04_soal1.java

---
+++ eval04_soal1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class eval04_soal1 {
+    public static void main(String[] args) {
+        Scanner w = new Scanner(System.in);
+
+        int a = w.nextInt();
+        int b = w.nextInt();
+        int c;
+        if (a >= b) {
+            c = a + b;
+            if (c % 3 == 0) {
+                for (int i = 1; i <= c; i++) {
+                    for (int j = 1; j <= i; j++) {
+                        System.out.print("* ");
+                    }
+                    System.out.println();
+                }
+            } else {
+                System.out.println(c);
+            }
+        } else {
+            c = a * b;
+            if (c % 2 == 0) {
+                for (int i = 1; i <= c; i++) {
+                    for (int j = 1; j <= c; j++) {
+                        System.out.print("* ");
+                    }
+                    System.out.println();
+                }
+            } else {
+                System.out.println(c);
+            }
+
+        }
+    }
+}
