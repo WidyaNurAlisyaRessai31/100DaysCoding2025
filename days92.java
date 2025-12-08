02964144456017893a860f21011ff7d7c473cd95
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-12-08 19:47:25 +0800

    days92.java

---
+++ days92.java
@@ -0,0 +1,10 @@
+public class days92 {
+    public static int tambah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int hasil = tambah(5, 7);
+        System.out.println("Hasil: " + hasil);
+    }
+}
