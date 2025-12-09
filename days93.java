7a704f7f6d503ce31ae8191475b476c65dfc0ef9
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-12-09 20:28:49 +0800

    days93.java

---
+++ days93.java
@@ -0,0 +1,10 @@
+public class days93 {
+    public static void main(String[] args) {
+        double angka = 7.45;
+
+        System.out.println("Angka: " + angka);
+        System.out.println("Math.ceil: " + Math.ceil(angka));
+        System.out.println("Math.floor: " + Math.floor(angka));
+        System.out.println("Math.round: " + Math.round(angka));
+    }
+}
