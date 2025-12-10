d1ce1273040490e015520dd1ae6c7ed16852ee32
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-12-10 20:30:55 +0800

    days94.java

---
+++ days94.java
@@ -0,0 +1,19 @@
+public class days94 {
+    public static void main(String[] args) {
+        salam();
+        cetakNama("Wiwi");
+        cetakAngka(5);
+    }
+
+    public static void salam() {
+        System.out.println("Hello, ini method void!");
+    }
+
+    public static void cetakNama(String nama) {
+        System.out.println("Nama: " + nama);
+    }
+
+    public static void cetakAngka(int angka) {
+        System.out.println("Angka: " + angka);
+    }
+}
