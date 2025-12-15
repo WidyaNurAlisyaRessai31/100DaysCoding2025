25e071a1aee28233de63dcdcdb6bccfab2e2ab49
WidyaNurAlisyaRessai31 <widyanuralisya.r@gmail.com>
2025-12-15 23:24:52 +0800

    days99.java

---
+++ days99.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class days99 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int n = sc.nextInt();
+
+        System.out.println("Bilangan prima dari 1 sampai " + n + " adalah:");
+
+        for (int i = 2; i <= n; i++) {
+            boolean prima = true;
+
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    prima = false;
+                    break;
+                }
+            }
+
+            if (prima) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
